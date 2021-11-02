bd948fae27ca39162d67c64c310ee13f24247028
Edoardo Remondini <edoardo.remondini@studenti.itsincom.it>
2021-11-02 17:43:49 +0100

    aggiunto campo everclciked per capire se bottone è mai stato cliccato

--- models/buttons.py
+++ models/buttons.py
@@ -2,6 +2,8 @@
 class Buttons():
     def __init__(Self,x,y,image):
         Self.image=image
+        #Self.rect=Self.image.get_rect()#per ottenere un rect da immagine che è una surface
         Self.x=x
         Self.y=y
+        Self.everclicked=False#se bottone non è mai stato cliccato sarà false
     
